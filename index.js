5af07b10c55d353ea304f6349bfbd9730e2a4ddb
Cristhian <cristhiangomezdev@gmail.com>
2023-02-26 15:21:35 +0100

    firts

---
+++ index.js
@@ -0,0 +1,24 @@
+const { request } = require("express");
+const express = require("express");
+const app = express();
+const PORT = 3010;
+
+const seat = require("./seat.json");
+
+app.get("/", (request, response)=>{
+    response.send({response: true, code: 200, seat: seat})
+} );
+
+app.get("/:id", (request, response)=>{
+const {id}= request.params;
+const results = seat.filter(seat => seat.id === Number(id))
+response.status(200).send({response: true, seat: results})
+})
+
+app.post("/create", (req, res)=>{
+    
+})
+
+app.listen(PORT, ()=>{
+console.log(`Server listening on port ${PORT}...`);
+});
